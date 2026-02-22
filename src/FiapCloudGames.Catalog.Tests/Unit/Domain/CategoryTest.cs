731ad38d0da3ce4dcd1f773ae9f82aabcf8fb385
Matheus Davila <mat.97@hotmail.com>
2026-02-22 18:39:09 -0300

    feat(43): Adicionado testes unitários de Category e Game

---
+++ src/FiapCloudGames.Catalog.Tests/Unit/Domain/CategoryTest.cs
@@ -0,0 +1,52 @@
+﻿using FiapCloudGames.Catalog.Domain.Entities;
+using FiapCloudGames.Catalog.Domain.Exceptions;
+
+namespace FiapCloudGames.Catalog.Tests.Unit.Domain;
+
+public class CategoryTest
+{
+    #region Name validations
+
+    [Fact]
+    public void MustFailWhenNameIsEmpty()
+    {
+        var exception = Assert.Throws<DomainException>(() =>
+            new Category("")
+        );
+
+        Assert.Equal("Categoria inválida. Deve ser informado.", exception.Message);
+    }
+
+    [Fact]
+    public void MustFailWhenNameIsTooShort()
+    {
+        var exception = Assert.Throws<DomainException>(() =>
+            new Category("a")
+        );
+
+        Assert.Equal("Categoria inválida. Deve conter no mínimo 2 caracteres.", exception.Message);
+    }
+
+    [Fact]
+    public void MustFailWhenNameIsTooLong()
+    {
+        var exception = Assert.Throws<DomainException>(() =>
+            new Category(new string('a', 41))
+        );
+
+        Assert.Equal("Categoria inválida. Excedeu a quantidade máxima de 40 caracteres.", exception.Message);
+    }
+
+    #endregion Name validations
+
+    [Theory]
+    [InlineData("RPG")]
+    [InlineData("2D")]
+    [InlineData("Esporte")]
+    public void MustBeCreatedWhenTheCategoryIsValid(string name)
+    {
+        var category = new Category(name);
+
+        Assert.Equal(category.Name, name);
+    }
+}
---
+++ src/FiapCloudGames.Catalog.Tests/Unit/Domain/GameTest.cs
@@ -0,0 +1,77 @@
+﻿using FiapCloudGames.Catalog.Domain.Entities;
+using FiapCloudGames.Catalog.Domain.Exceptions;
+
+namespace FiapCloudGames.Catalog.Tests.Unit.Domain;
+
+public class GameTest
+{
+    
+    #region Title validations
+
+    [Fact]
+    public void MustFailWhenTitleIsEmpty()
+    {
+        var exception = Assert.Throws<DomainException>(() =>
+            new Game("", [])
+        );
+
+        Assert.Equal("Título inválido. Deve ser informado.", exception.Message);
+    }
+
+    [Fact]
+    public void MustFailWhenTitleIsTooLong()
+    {
+        var exception = Assert.Throws<DomainException>(() =>
+            new Game(new string('a', 81), [])
+        );
+
+        Assert.Equal("Título inválido. Excedeu a quantidade máxima de 80 caracteres.", exception.Message);
+    }
+
+    #endregion Title validations
+
+    #region Category validations
+
+    [Fact]
+    public void MustFailWhenCategoryIsEmpty()
+    {
+        var exception = Assert.Throws<DomainException>(() =>
+            new Game(new string('a', 50), [])
+        );
+
+        Assert.Equal("O jogo deve ter pelo menos uma categoria.", exception.Message);
+    }
+
+    [Fact]
+    public void MustFailWhenDuplicateCategories()
+    {
+        var category = new Category("RPG");
+
+        var categories = new List<Category>
+        {
+            category,
+            category
+        };
+
+        var exception = Assert.Throws<DomainException>(() =>
+            new Game(new string('a', 50), categories)
+        );
+
+        Assert.Equal("Categoria duplicada não é permitida.", exception.Message);
+    }
+
+    #endregion Category validations
+
+    [Theory]
+    [InlineData("Age of Empires", "Ação")]
+    [InlineData("Fifa 2026", "RPG")]
+    [InlineData("CS", "Estratégia")]
+    public void MustBeCreatedWhenTheGameIsValid(string title, string categoryName)
+    {
+        var category = new Category(categoryName);
+
+        var game = new Game(title, [category]);
+
+        Assert.Equal(game.Title, title);
+    }
+}
