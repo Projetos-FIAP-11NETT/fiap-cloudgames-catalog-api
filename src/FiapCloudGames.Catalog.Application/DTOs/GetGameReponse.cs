fc4601f23b00f53a8e862902d200d0961b6d5776
Matheus Davila <mat.97@hotmail.com>
2026-03-10 22:30:59 -0300

    feat(43): Adicionado Controller e DTO de Game

---
+++ src/FiapCloudGames.Catalog.Application/DTOs/GetGameReponse.cs
@@ -0,0 +1,12 @@
+﻿namespace FiapCloudGames.Catalog.Application.DTOs;
+
+public class GetGameReponse
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime ReleaseDate { get; set; }
+    public string Developer { get; set; }
+    public decimal Price { get; set; }
+    public List<Guid> Categories { get; set; }
+}
