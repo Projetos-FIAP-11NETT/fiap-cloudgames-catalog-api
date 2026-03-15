dda9ab03c2ee2146232a550057031fbbab44d8a5
Matheus Davila <mat.97@hotmail.com>
2026-03-15 02:58:11 -0300

    feat(43): Adicionado command para alterar status do pedido

---
+++ src/FiapCloudGames.Catalog.Application/OrderFeature/Commands/UpdateOrderStatus/UpdateOrderStatusCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using FiapCloudGames.Catalog.Domain.Contracts.Repositories;
+using MediatR;
+
+namespace FiapCloudGames.Catalog.Application.OrderFeature.Commands.UpdateOrderStatus;
+
+public class UpdateOrderStatusCommandHandler (IOrderRepository orderRepository) : IRequestHandler<UpdateOrderStatusCommand, bool>
+{
+    public async Task<bool> Handle(UpdateOrderStatusCommand request, CancellationToken cancellationToken)
+    {
+        var order = await orderRepository.GetOrderByIdAsync(request.OrderId);
+        if (order is null)
+            return false;
+        
+        order.UpdateStatus(request.Status);
+        orderRepository.Update(order);
+        await orderRepository.SaveChangesAsync(cancellationToken);
+        return true;
+    }
+}
