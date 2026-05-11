31d57b6f4edf9d2b6c270a51d9519fdbc6dfb4a1
Jediel <jediel.dev@gmail.com>
2026-05-11 20:38:58 -0300

    feat: Implementando envio de e-mail

---
+++ src/FiapCloudGames.Queue/Publishers/EmailNotificationPublisher.cs
@@ -0,0 +1,35 @@
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using FiapCloudGames.Catalog.Domain.Contracts.Publishers;
+using FiapCloudGames.Queue.Configurations.Sqs;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Text.Json;
+
+namespace FiapCloudGames.Queue.Publishers;
+
+public class EmailNotificationPublisher(
+    IAmazonSQS sqsClient,
+    IOptions<SqsSettings> sqsSettings,
+    ILogger<EmailNotificationPublisher> logger) : IEmailNotificationPublisher
+{
+    public async Task PublishAsync(string to, string subject, string body, CancellationToken cancellationToken = default)
+    {
+        logger.LogInformation(
+            "[catalog-service] Publishing email notification to SQS: To={To}, Subject={Subject}",
+            to, subject);
+
+        var messageBody = JsonSerializer.Serialize(new
+        {
+            To = to,
+            Subject = subject,
+            Body = body
+        });
+
+        await sqsClient.SendMessageAsync(new SendMessageRequest
+        {
+            QueueUrl = sqsSettings.Value.EmailQueueUrl,
+            MessageBody = messageBody
+        }, cancellationToken);
+    }
+}
